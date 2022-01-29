98fde9b6d5ef960874e0b70e693d2bd49351e3a5
basha <69071214+basha48@users.noreply.github.com>
2022-01-29 18:54:45 +0530

    Logical And Junit Programs

--- src/JUnitTesting/MonthlyPayment.java
+++ src/JUnitTesting/MonthlyPayment.java
@@ -18,14 +18,7 @@ public class MonthlyPayment {
 		
 		Utilitymethod.Monthlypayment(p, y, r);
 	}
-	public static void Sqrt(int c) {
-		 double epsilon = 1e-15;    
-	        double t = c;              
-	        while (Math.abs(t - c/t) > epsilon*t) {
-	            t = (c/t + t) / 2.0;
-	        }
-	        System.out.println(t);
-	}
+	
 	
 	
 	
